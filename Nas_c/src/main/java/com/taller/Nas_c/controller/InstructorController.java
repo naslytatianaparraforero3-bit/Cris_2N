52b9c551de691bd8ac2bbeacf8ab2f4ff193417a
Nasly Parra <naslytatianaparraforero3@gmail.com>
2026-06-17 09:15:32 -0500

    se crean los controller instru y programa

--- Nas_c/src/main/java/com/taller/Nas_c/controller/InstructorController.java
+++ Nas_c/src/main/java/com/taller/Nas_c/controller/InstructorController.java
@@ -1,5 +1,21 @@
 package com.taller.Nas_c.controller;
 
+import com.taller.Nas_c.service.EstadisticasService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class InstructorController {
-    
+
+    private final EstadisticasService estadisticas;
+
+    public InstructorController(EstadisticasService estadisticas) {
+        this.estadisticas = estadisticas;
+    }
+
+    @GetMapping("/instructores")
+    public String consultarInstructores() {
+        estadisticas.incrementarInstructores();
+        return "Listado de instructores";
+    }
 }
--- Nas_c/src/main/java/com/taller/Nas_c/controller/ProgramaController.java
+++ Nas_c/src/main/java/com/taller/Nas_c/controller/ProgramaController.java
@@ -1,5 +1,22 @@
 package com.taller.Nas_c.controller;
 
+import com.taller.Nas_c.service.EstadisticasService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProgramaController {
-    
+
+    private final EstadisticasService estadisticas;
+
+    public ProgramaController(EstadisticasService estadisticas) {
+        this.estadisticas = estadisticas;
+    }
+
+    @GetMapping("/programas")
+    public String consultarProgramas() {
+        estadisticas.incrementarProgramas();
+        return "Listado de programas de formación";
+    }
 }
+
