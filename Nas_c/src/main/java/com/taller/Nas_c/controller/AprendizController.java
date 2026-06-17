a906c2bcfee2ce340bf575989712f632f1be8d6a
Nasly Parra <naslytatianaparraforero3@gmail.com>
2026-06-17 08:56:01 -0500

    se crea el service y el controller aprendiz

--- Nas_c/src/main/java/com/taller/Nas_c/controller/AprendizController.java
+++ Nas_c/src/main/java/com/taller/Nas_c/controller/AprendizController.java
@@ -1,5 +1,21 @@
 package com.taller.Nas_c.controller;
 
+import com.taller.Nas_c.service.EstadisticasService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AprendizController {
-    
+
+    private final EstadisticasService estadisticas;
+
+    public AprendizController(EstadisticasService estadisticas) {
+        this.estadisticas = estadisticas;
+    }
+
+    @GetMapping("/aprendices")
+    public String consultarAprendices() {
+        estadisticas.incrementarAprendices();
+        return "Listado de aprendices";
+    }
 }
--- Nas_c/src/main/java/com/taller/Nas_c/service/EstadisticasService.java
+++ Nas_c/src/main/java/com/taller/Nas_c/service/EstadisticasService.java
@@ -1,5 +1,43 @@
 package com.taller.Nas_c.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class EstadisticasService {
-    
+
+    private int totalAprendices;
+    private int totalInstructores;
+    private int totalProgramas;
+
+    public EstadisticasService() {
+        System.out.println("Instancia de EstadisticasService creada: " + this.hashCode());
+    }
+
+    public void incrementarAprendices() {
+        totalAprendices++;
+    }
+
+    public void incrementarInstructores() {
+        totalInstructores++;
+    }
+
+    public void incrementarProgramas() {
+        totalProgramas++;
+    }
+
+    public int obtenerTotalConsultas() {
+        return totalAprendices + totalInstructores + totalProgramas;
+    }
+
+    public int getTotalAprendices() {
+        return totalAprendices;
+    }
+
+    public int getTotalInstructores() {
+        return totalInstructores;
+    }
+
+    public int getTotalProgramas() {
+        return totalProgramas;
+    }
 }
