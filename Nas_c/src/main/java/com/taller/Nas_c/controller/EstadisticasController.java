27b618d3d78aae0b950e26acfcbdd983fb9dc1ac
Nasly Parra <naslytatianaparraforero3@gmail.com>
2026-06-17 09:24:35 -0500

    se crea el controller estadis como archivo final

--- Nas_c/src/main/java/com/taller/Nas_c/controller/EstadisticasController.java
+++ Nas_c/src/main/java/com/taller/Nas_c/controller/EstadisticasController.java
@@ -1,5 +1,23 @@
 package com.taller.Nas_c.controller;
 
+import com.taller.Nas_c.service.EstadisticasService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EstadisticasController {
-    
+
+    private final EstadisticasService estadisticas;
+
+    public EstadisticasController(EstadisticasService estadisticas) {
+        this.estadisticas = estadisticas;
+    }
+
+    @GetMapping("/estadisticas")
+    public String mostrarEstadisticas() {
+        return "Total de consultas: " + estadisticas.obtenerTotalConsultas()
+                + " (Aprendices: " + estadisticas.getTotalAprendices()
+                + ", Instructores: " + estadisticas.getTotalInstructores()
+                + ", Programas: " + estadisticas.getTotalProgramas() + ")";
+    }
 }
